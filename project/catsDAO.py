9977e504d1b058c689677b9c3abf847c92e7ba14
caioforteribeiro <caio.forteribeiro@gmail.com>
2023-12-02 00:34:07 +0000

    modified catsDAO

--- project/catsDAO.py
+++ project/catsDAO.py
@@ -1,26 +1,52 @@
 import mysql.connector
+import dbconfig as cfg
+
+
 class catsDAO:
-    db=""
-    def __init__(self): 
-        self.db = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="datarepresentation"
+    connection=""
+    cursor =""
+    host=""
+    user=""
+    password=""
+    database=""
+
+    #Initialise
+    def __init__(self):
+        self.host=cfg.mysql['host']
+        self.user=cfg.mysql['user']
+        self.password=cfg.mysql['password']
+        self.database=cfg.mysql['database']
+
+    #Cursor
+    def getcursor(self): 
+        self.connection = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database,
         )
+        self.cursor = self.connection.cursor()
+        return self.cursor
+    
+    #Close connection and cursor
+    def closeAll(self):
+        self.connection.close()
+        self.cursor.close()
 
     #Create
     def create(self, values):
-        cursor = self.db.cursor()
-        sql="insert into cats (name, age, breed) values (%s,%s,%s)"
+        cursor = self.getcursor()
+        sql="insert into cats (name,age,breed) values (%s,%s,%s)"
         cursor.execute(sql, values)
 
-        self.db.commit()
-        return cursor.lastrowid
+        self.connection.commit()
+        newid = cursor.lastrowid
+        self.closeAll()
+        return newid
     
     #Get all
     def getAll(self):
-        cursor = self.db.cursor()
+        cursor = self.getcursor()
         sql="select * from cats"
         cursor.execute(sql)
         results = cursor.fetchall()
@@ -29,38 +55,45 @@ class catsDAO:
         for result in results:
             print(result)
             returnArray.append(self.convertToDictionary(result))
+        
+        self.closeAll()
         return returnArray
     
     #Find by ID
     def findByID(self, id):
-        cursor = self.db.cursor()
-        sql="select * from cats where id = %s"
+        cursor = self.getcursor()
+        sql="select * from cats where id=%s"
         values = (id,)
 
         cursor.execute(sql, values)
         result = cursor.fetchone()
-        return self.convertToDictionary(result)
+        returnvalue = self.convertToDictionary(result)
+        self.closeAll()
+        return returnvalue
     
     #Update
     def update(self, values):
-        cursor = self.db.cursor()
-        sql="update cats set name= %s, age=%s, breed=%s  where id = %s"
+        cursor = self.getcursor()
+        sql="update cats set name= %s,age=%s,breed=%s  where id=%s"
         cursor.execute(sql, values)
-        self.db.commit()
+        self.connection.commit()
+        self.closeAll()
 
     #Delete
     def delete(self, id):
-        cursor = self.db.cursor()
-        sql="delete from cats where id = %s"
+        cursor = self.getcursor()
+        sql="delete from cats where id=%s"
         values = (id,)
 
         cursor.execute(sql, values)
 
-        self.db.commit()
+        self.connection.commit()
+        self.closeAll()
+        
         print("delete done")
 
     def convertToDictionary(self, result):
-        colnames=['id','name','age','breed']
+        colnames=["id","name","age","breed"]
         item = {}
         
         if result:
